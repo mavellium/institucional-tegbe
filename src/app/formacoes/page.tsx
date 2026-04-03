75faa86e9a874840bb68c3209cca99d0ac5c1c9d
viniciustmota <motaviny140@gmail.com>
2026-04-03 09:43:59 -0300

    fix: corrigindo LCP

--- src/app/formacoes/page.tsx
+++ src/app/formacoes/page.tsx
@@ -56,14 +56,7 @@ export default async function FormacoesPage() {
   return (
     <>
       {lcpImageUrl && (
-        <link
-          rel="preload"
-          as="image"
-          href={`/_next/image?url=${encodeURIComponent(lcpImageUrl)}&w=1080&q=60`}
-          imageSrcSet={`/_next/image?url=${encodeURIComponent(lcpImageUrl)}&w=640&q=60 640w, /_next/image?url=${encodeURIComponent(lcpImageUrl)}&w=750&q=60 750w, /_next/image?url=${encodeURIComponent(lcpImageUrl)}&w=828&q=60 828w, /_next/image?url=${encodeURIComponent(lcpImageUrl)}&w=1080&q=60 1080w`}
-          imageSizes="100vw"
-          fetchPriority="high"
-        />
+        <link rel="preload" as="image" href={lcpImageUrl} imageSizes="100vw" fetchPriority="high" />
       )}
       <Schema
         data={{
