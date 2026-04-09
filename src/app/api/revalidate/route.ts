fc9fb6a784d6738a4504adb00ff8b2553bf8bf91
viniciustmota <motaviny140@gmail.com>
2026-04-09 10:21:14 -0300

    remover rotas duplicadas

--- src/app/api/revalidate/route.ts
+++
@@ -1,8 +0,0 @@
-import { revalidateTag } from "next/cache";
-
-export async function POST() {
-  console.log("REVALIDANDO CACHE");
-  revalidateTag("cms", "max");
-
-  return Response.json({ ok: true });
-}
